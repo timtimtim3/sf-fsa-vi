73b71d5c13cf737b17e62eb6a97cefe609b64de1
Guillermo <guillermo.infante@upf.edu>
2024-07-22 11:10:16 +0200

    added utils folder

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+import random, os
+import torch as th  
+import numpy as np 
+import os
+
+def __init__():
+    pass
+
+
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    th.manual_seed(seed)
+    th.cuda.manual_seed(seed)
+    th.backends.cudnn.deterministic = True
+    th.backends.cudnn.benchmark = True
