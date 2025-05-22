541d30937ed76a7f3c692926832d1fa1d7ef5307
timtimtim3 <68196410+timtimtim3@users.noreply.github.com>
2025-05-22 15:12:14 +0200

    update loading of OptionDQN and MetaPolicyDQN and tabular options so they load into existing policies created from __init__

--- play_lof.py
+++ play_lof.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+from hydra.utils import get_class
 from omegaconf import DictConfig, OmegaConf
 from fsa.tasks_specification import load_fsa
 from envs.wrappers import GridEnvWrapper
@@ -15,10 +18,19 @@ def main(cfg: DictConfig) -> None:
 
     seed_everything(cfg.seed)
 
-    # env setup
-    env_name = cfg.env.env_name
-    train_env = gym.make(env_name)
-    eval_env = gym.make(env_name)
+    env_params = dict(cfg.env)
+    env_name = env_params.pop("env_name")
+
+    train_env_kwargs = deepcopy(env_params)
+    train_env_kwargs.pop("restriction")
+    train_env_kwargs.pop("planning_constraint")
+
+    excluded_keys = {"add_obj_to_start", "add_empty_to_start", "reset_probability_goals"}
+    eval_env_kwargs = {k: v for k, v in train_env_kwargs.items() if k not in excluded_keys}
+
+    # Load the environments (train and eval)
+    train_env = gym.make(env_name, **train_env_kwargs)
+    eval_env = gym.make(env_name, **eval_env_kwargs)
 
     # Directory for storing the policies
     directory = train_env.unwrapped.spec.id
@@ -32,24 +44,16 @@ def main(cfg: DictConfig) -> None:
     fsa, T = load_fsa(f"{env_name}-{fsa_task}", eval_env)
     eval_env = GridEnvWrapper(eval_env, fsa, fsa_init_state="u0", T=T)
 
-    # load the meta‐policy that you originally saved
-    # pick the right class (e.g. VI or QLearning) to match what you ran
-    meta: MetaPolicyVI = MetaPolicyVI.load(
-        env=train_env,
-        eval_env=eval_env,
-        fsa=fsa,
-        T=T,
-        base_dir=base_save_dir,
-        gamma=cfg.algorithm.gamma,  # or hardcode if needed
-    )
+    lof = hydra.utils.call(config=cfg.algorithm, env=train_env, eval_env=eval_env, fsa=fsa, T=T)
+    lof.load(base_dir=base_save_dir)
 
     # now you can evaluate, visualize, etc.
-    success, reward = meta.evaluate_metapolicy(reset=False)
+    success, reward = lof.evaluate_metapolicy(reset=False)
     print(f"Success={success}, Reward={reward}")
     # … any other analysis …
 
-    meta.plot_meta_qvals(base_dir=base_save_dir)
-    meta.plot_q_vals(base_dir=os.path.join(base_save_dir, "options"))
+    lof.plot_meta_qvals(base_dir=base_save_dir)
+    lof.plot_q_vals(base_dir=os.path.join(base_save_dir, "options"))
 
 
 if __name__ == "__main__":
